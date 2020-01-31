c984ab0cc7b464bbd0fd3189d0384f8e00c26366
Zoulouti <Pauldelaubadere@gmail.com>
2020-02-01 00:25:22 +0100

    add ProceduralGeneration

---
+++ GlobalGameJam2020/Assets/Scripts/Input_Scripts/InputReferenceScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputReferenceScript : MonoBehaviour
+{
+    public Image m_childImage;
+
+    public string input;
+
+    string[] allPossibleInputs;
+
+    private void Start()
+    {
+        allPossibleInputs = new string[8] { "a", "b", "x", "y", "up", "right", "down", "left" };
+    }
+}
